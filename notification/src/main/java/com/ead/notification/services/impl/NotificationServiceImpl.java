68a9ffbdf60d791cbb3e7477e469d40b084a9349
allanCruzX <allancruzrosa@gmail.com>
2022-04-24 11:48:02 -0300

    Comunicação Assíncrona via Command Messages

---
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ead.notification.services.impl;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.ead.notification.models.NotificationModel;
+import com.ead.notification.repositories.NotificationRepository;
+import com.ead.notification.services.NotificationService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    final NotificationRepository notificationRepository;
+
+    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public NotificationModel saveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
+    }
+
+    @Override
+    public Page<NotificationModel> findAllNotificationsByUser(UUID userId, Pageable pageable) {
+        return notificationRepository.findAllByUserIdAndNotificationStatus(userId, NotificationStatus.CREATED,pageable);
+    }
+
+    @Override
+    public Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId) {
+        return notificationRepository.findByNotificationIdAndUserId(notificationId, userId);
+    }
+}
